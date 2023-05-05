95148303b0c81614598c30ad97bd26cb9fa906dd
yureyx <fachrifadhilahsanum22@gmail.com>
2023-05-05 16:54:33 +0700

    add: ref #16, #17, #18, #25, #26, #27

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+Route::get('/store' , [\App\Http\Controllers\LandingController::class , 'store'])->name('store');
+Route::post('/cart/add', [\App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
+Route::get('/cart' , [\App\Http\Controllers\CartController::class , 'index'])->name('cart');
+Route::put('/cart/{id}', [\App\Http\Controllers\CartController::class , 'updateQuantity'])->name('cart.update');
+Route::delete('/cart/{id}', [\App\Http\Controllers\CartController::class , 'destroy'])->name('cart.destroy');
+Route::post('order' , [\App\Http\Controllers\CartController::class , 'order'])->name('order');
+Route::get('/order/list' , [\App\Http\Controllers\CartController::class , 'orderList'])->name('order.list');
+Route::prefix('admin')->middleware('admin')->group(function (){
+    Route::get('/orderlist' , [\App\Http\Controllers\AdminController::class , 'orderList'])->name('admin.orderList');
+    Route::put('/orders/cancel/{id}', [\App\Http\Controllers\AdminController::class, 'cancelOrder'])->name('orders.cancel');
+    Route::put('/orders/send/{id}', [\App\Http\Controllers\AdminController::class, 'sendOrder'])->name('orders.send');
+});
